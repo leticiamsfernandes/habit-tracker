08c940217723084b74ff0d3ad988242ed0bc180b
Letícia Fernandes <lemayara16@gmail.com>
2023-01-20 17:48:23 -0300

    Avançando o front-end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,16 @@
-import './styles/global.css'
+import "./styles/global.css";
 
-import { Habit } from "./components/Habit"
+import { HabitDay } from "./components/HabitDay";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={3}/>
-      <Habit completed={3}/>
-      <Habit completed={3}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
-    
-  )
+  );
 }
-
-export default App
